0f6c11f08f745f0544c264e2a8811031cd5b903c
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-10-18 19:29:14 +0700

    membuat Tulisan Login pada bagain atas

--- app/src/main/java/com/example/basiccomposable/Biodataku.kt
+++ app/src/main/java/com/example/basiccomposable/Biodataku.kt
@@ -1,11 +1,24 @@
 package com.example.basiccomposable
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Biodataku(modifier: Modifier = Modifier) {
@@ -17,5 +30,8 @@ fun Biodataku(modifier: Modifier = Modifier) {
             .padding(top = 50.dp)
             .background(color = Color(0xFFDCDCDC))
     )
+    {
+        Text("Login", fontSize = 50.sp, fontWeight = FontWeight.Bold)
+    }
 
 }
