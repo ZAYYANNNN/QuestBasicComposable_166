7872aa98fad1a89cad1bc2d69b52d6bdf971f286
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-10-18 19:20:49 +0700

    membuat column yang diatur di tengah secara horiziontal

--- app/src/main/java/com/example/basiccomposable/Biodataku.kt
+++ app/src/main/java/com/example/basiccomposable/Biodataku.kt
@@ -1,9 +1,21 @@
 package com.example.basiccomposable
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Biodataku(modifier: Modifier = Modifier) {
+    Column  (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+            .background(color = Color(0xFFDCDCDC))
+    )
 
 }
