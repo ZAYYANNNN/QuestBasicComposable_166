d9fa4fad5a8b01ac6b685a9d5a003785504ceca9
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-10-18 19:10:55 +0700

    Push file

---
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.basiccomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.basiccomposable.ui.theme.BasicComposableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BasicComposableTheme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Biodataku(modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicColumn(modifier: Modifier = Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+    )
+    {
+        Text("Hello", fontSize = 100.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text("Jetpack Compose",
+            fontSize = 50.sp,
+            fontStyle = FontStyle.Italic)
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape)
+        )
+    }
+}
+
